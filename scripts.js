77cc8d1f51fa5b3b6d2af6365c7f4475827ef4da
Harshit Bahuguna <harshitbahuguna2002@gmail.com>
2024-10-12 13:24:51 +0530

    added winner alert and reset game functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard=false;
 let firstCard, secondCard;
+let total = 0;
+
 function flipCard() {
   if (lockBoard) return;
   if(this===firstCard) return;
@@ -11,32 +13,33 @@ function flipCard() {
     firstCard = this;
     return;
   }
-    hasFlippedCard = false;
-    secondCard = this;
-    checkForMatch();
-  }
-
-
-
-function checkForMatch() {
-  let isMatch= firstCard.dataset.frame === 
-  secondCard.dataset.frame;
-isMatch ? disableCards() : unflipCards();
+  hasFlippedCard = false;
+  secondCard = this;
+  checkForMatch();
 }
 
-
+function checkForMatch() {
+  let isMatch = firstCard.dataset.frame === 
+  secondCard.dataset.frame;
+  isMatch ? disableCards() : unflipCards();
+}
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  total += 2;
   resetBoard();
+  if (total === 12) {
+    setTimeout(() => {
+      alert("Congratulations!! you've a sharp memory.");
+      initialize();
+      resetBoard();
+    }, 1000);
+  }
 }
-
-
 
 function unflipCards() {
   lockBoard=true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
@@ -44,16 +47,23 @@ function unflipCards() {
   }, 1500);
 }
 
-function resetBoard(){
-[hasFlippedCard,lockBoard]=[false,false];
-[firstCard,secondCard]=[null,null];
+function resetBoard() {
+  [hasFlippedCard,lockBoard]=[false,false];
+  [firstCard,secondCard]=[null,null];
 }
 
-(function shuffle(){
+function shuffle(){
   cards.forEach(card=>{
     let randomPos=Math.floor(Math.random()*12);
     card.style.order=randomPos;
   });
-})();
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function initialize() {
+  total = 0;
+  cards.forEach(card => card.classList.remove('flip'));
+  shuffle();
+  cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+initialize();
